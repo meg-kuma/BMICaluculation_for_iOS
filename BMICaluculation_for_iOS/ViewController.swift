64b74a96cebfa89b949cde23406497d132a3596c
meg-kuma <syou.canopus.6104@gmail.com>
2017-04-29 12:59:14 +0900

    Application creation challenge 1

--- BMICaluculation_for_iOS/ViewController.swift
+++ BMICaluculation_for_iOS/ViewController.swift
@@ -8,16 +8,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITextFieldDelegate {
+    
+    @IBOutlet var bmiHeight: UITextField!
+    @IBOutlet var bmiWeight: UITextField!
+    @IBOutlet var bmiLabel: UILabel!
+    @IBOutlet var bmi: UILabel!
+    
+    var height:Float = 0.0
+    var weight:Float = 0.0
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        bmiLabel.isHidden = true
+        bmi.isHidden = true
+        
+        bmiHeight.delegate = self
+        bmiWeight.delegate = self
+        
+        
     }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        bmiHeight.resignFirstResponder()
+        bmiWeight.resignFirstResponder()
+        
+        return true
+    }
+    
+    @IBAction func tap(_ sender: Any) {
+        
+        height = Float(bmiHeight.text!)!
+        weight = Float(bmiWeight.text!)!
+        
+        height = height / 100
+        
+        let bmi_anser:Float = (weight / (height * height)) * 10
+        
+        bmi.text = String(round(bmi_anser) / 10)
+        
+        bmiLabel.isHidden = false
+        bmi.isHidden = false
+        
+        
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        
+        
     }
 
 
